12d9c86adee39d6b5a446453fceae3d42091c48a
gianmtedeschi <giam.tedeschi@gmail.com>
2026-06-02 21:41:20 +0200

    minor changes, common requirements

---
+++ RL-basics/envs/__init__.py
@@ -0,0 +1,8 @@
+from envs.custom_cliff import CliffWalkingEnv
+from envs.taxi_utils import render_taxi, close_taxi_render
+
+__all__ = [
+    "CliffWalkingEnv",
+    "render_taxi",
+    "close_taxi_render",
+]
